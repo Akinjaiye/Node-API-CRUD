ce3a5687a0351e4e8a454deb05ec1d5820b3382e
Akinjaiye <akindijijaiyejaiye@gmail.com>
2024-08-30 12:19:24 +0100

    add controller folder

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,3 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const Product = require("../models/product.model.js")
+const { getProducts,getAProduct, createProduct, updateProduct, deleteProduct } = require("../controllers/product.controller.js")
 
+router.get("/", getProducts);
+
+router.get("/:id", getAProduct);
+
+router.post("/", createProduct);
+  
+router.put("/:id", updateProduct);
+
+router.delete("/:id", deleteProduct);
+
+  module.exports = router;
