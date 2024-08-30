e831c1725a39065ffbd3c922d170d1ed01978f28
Akinjaiye <akindijijaiyejaiye@gmail.com>
2024-08-30 11:50:56 +0100

    add router folder

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const Product = require("./models/product.model.js");
 const app = express();
 
+
+//middleware
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.get("/", (req, res) => {
   res.send("Hello from Node API");
@@ -67,6 +70,10 @@ app.delete("/api/products/:id", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+//routes
+app.use("/api/products", productRoute)
+
 
 mongoose
   .connect(
